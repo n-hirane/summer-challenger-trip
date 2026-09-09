5269a26a0857d91481efe01dab547090de449f08
n-hirane <57578828+n-hirane@users.noreply.github.com>
2026-09-09 12:11:25 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE="summer-challenger-v7";
+const CACHE="summer-challenger-v8";
 const CORE=[
   "./",
   "./index.html",
-  "./style.css?v=7",
-  "./app.js?v=7",
+  "./style.css?v=8",
+  "./app.js?v=8",
   "./manifest.webmanifest"
 ];
 const STATIC=[
